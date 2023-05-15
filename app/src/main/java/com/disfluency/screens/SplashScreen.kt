12cc85003ef0e0ec79135022bc53f137f5946ee0
AgustinCragno <cragno.agustin@gmail.com>
2023-05-15 18:07:27 -0300

    Added Splash logo animation

--- app/src/main/java/com/disfluency/screens/SplashScreen.kt
+++ app/src/main/java/com/disfluency/screens/SplashScreen.kt
@@ -1,21 +1,22 @@
 package com.disfluency.screens
 
-import android.window.SplashScreen
+import androidx.compose.animation.*
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.disfluency.R
-import com.disfluency.navigation.BottomNavigation
 import com.disfluency.navigation.BottomNavigationItem
 import kotlinx.coroutines.delay
 
@@ -30,18 +31,31 @@ fun SplashScreen(navController: NavController){
     Splash()
 }
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
 fun Splash(){
+
+    val image = AnimatedImageVector.animatedVectorResource(R.drawable.disfluency_logo_animation)
+    var atEnd by remember { mutableStateOf(false) }
+
     Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Image(
-            painter = painterResource(id = R.drawable.disfluency_logo),
+            painter = rememberAnimatedVectorPainter(animatedImageVector = image, atEnd = atEnd),
             contentDescription = "Disfluency",
             modifier = Modifier.size(170.dp, 170.dp)
         )
+    }
+    
+    LaunchedEffect(Unit){
+        //TODO: esta ok?
+        while (true) {
+            delay(800)
+            atEnd = !atEnd
+        }
     }
 }
 
