8957e01f96099db2be29c9b5c8fbb4f0448dd6fc
AgustinCragno <cragno.agustin@gmail.com>
2023-05-15 18:23:45 -0300

    Splash screen (#2)
    * Added Splash Screen with app logo
    
    * Added Splash logo animation
    
    ---------
    
    Co-authored-by: JulianSima <juliansimaro@gmail.com>
    Co-authored-by: matiasanz <matias.l.anzorandia@gmail.com>

---
+++ app/src/main/java/com/disfluency/AppScaffold.kt
@@ -0,0 +1,46 @@
+package com.disfluency
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.disfluency.navigation.BottomNavigation
+import com.disfluency.navigation.NavigationGraph
+import com.disfluency.navigation.getItemByRoute
+
+@Composable
+fun AppScaffold() {
+    val navController = rememberNavController()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = getItemByRoute(currentRoute).title) },
+                navigationIcon = { Icon(Icons.Filled.Menu , contentDescription = "") },
+                actions = { Icon(Icons.Filled.AccountCircle, contentDescription = "") }
+            )
+        },
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        },
+        content = { paddingValues ->
+            Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+                NavigationGraph(navController = navController)
+            }
+        }
+    )
+}
