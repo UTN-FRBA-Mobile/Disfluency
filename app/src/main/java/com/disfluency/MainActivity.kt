7c1db885f272be8d686dc608af47b648154bc6ff
AgustinCragno <cragno.agustin@gmail.com>
2023-05-15 16:43:40 -0300

    Added Splash Screen with app logo

--- app/src/main/java/com/disfluency/MainActivity.kt
+++ app/src/main/java/com/disfluency/MainActivity.kt
@@ -4,26 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.disfluency.navigation.BottomNavigation
-import com.disfluency.navigation.NavigationGraph
-import com.disfluency.navigation.getItemByRoute
+import com.disfluency.navigation.AppNavigation
 import com.disfluency.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp(content = { ScaffoldTest()} )
+            MyApp(content = { AppNavigation() } )
         }
     }
 }
@@ -41,31 +34,8 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Composable
-fun ScaffoldTest() {
-    val navController = rememberNavController()
-
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
-
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(text = getItemByRoute(currentRoute).title) },
-                navigationIcon = { Icon(Icons.Filled.Menu , contentDescription = "") },
-                actions = { Icon(Icons.Filled.AccountCircle, contentDescription = "") }
-            )
-        },
-        bottomBar = {
-            BottomNavigation(navController = navController)
-        }
-    ) {
-        NavigationGraph(navController = navController)
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApp(content = { ScaffoldTest()} )
+    MyApp(content = { AppNavigation() } )
 }
---
+++ app/src/main/java/com/disfluency/navigation/AppNavigation.kt
@@ -0,0 +1,22 @@
+package com.disfluency.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.disfluency.AppScaffold
+import com.disfluency.screens.SplashScreen
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "splash"){
+        composable("splash"){
+            SplashScreen(navController = navController)
+        }
+        composable(BottomNavigationItem.Home.screenRoute){
+            AppScaffold()
+        }
+    }
+}
