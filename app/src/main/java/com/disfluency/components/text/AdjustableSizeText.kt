44142e38a9c5a6e4422767c439997e187f28f098
AgustinCragno <cragno.agustin@gmail.com>
2023-06-14 10:13:30 -0300

    improvement/bottom-nav-bar-responsive-text (#7)
    * Modified Bottom Navigation items' text to adjust size when any of the items is too long
    
    * Added wrapper class for adjustable size text units

---
+++ app/src/main/java/com/disfluency/components/text/AdjustableSizeText.kt
@@ -0,0 +1,34 @@
+package com.disfluency.components.text
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+
+const val TEXT_SCALE_REDUCTION_INTERVAL = 0.9f
+
+class AdjustableSizeUnit(size: TextUnit){
+    var textSize by mutableStateOf(size)
+        private set
+
+    fun scale(value: Float){
+        textSize = textSize.times(value)
+    }
+}
+
+@Composable
+fun AdjustableSizeText(text: String, adjustableSize: AdjustableSizeUnit){
+    Text(
+        text = text,
+        fontSize = adjustableSize.textSize,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        onTextLayout = { textLayoutResult ->
+            val maxCurrentLineIndex: Int = textLayoutResult.lineCount - 1
+
+            if (textLayoutResult.isLineEllipsized(maxCurrentLineIndex)) {
+                adjustableSize.scale(TEXT_SCALE_REDUCTION_INTERVAL)
+            }
+        }
+    )
+}
