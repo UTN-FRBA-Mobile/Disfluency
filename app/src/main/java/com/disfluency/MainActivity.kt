20077a91c0700eeae263e398ed8360a4c962fc76
josebruzzoni <bruzzonijose@gmail.com>
2023-07-05 23:11:44 -0300

    push notif wip

--- app/src/main/java/com/disfluency/MainActivity.kt
+++ app/src/main/java/com/disfluency/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.disfluency
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.datastore.preferences.core.stringPreferencesKey
 import com.disfluency.navigation.AppNavigation
 import com.disfluency.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 //TODO: migrar a ingles en todo lo que dejamos en español
 
@@ -21,10 +28,25 @@ class MainActivity : ComponentActivity() {
             MyApp(content = { AppNavigation() } )
         }
     }
+
+
 }
 
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    val gckTokenKey = stringPreferencesKey("gcm_token")
+    val fcmToken = flow<String> {
+        context.dataStore.data.map {
+            it[gckTokenKey]
+        }.collect(collector = {
+            if (it != null){
+                this.emit(it)
+            }
+        })
+    }.collectAsState(initial = "")
+    Log.v("FCMTOKEN", fcmToken.value)
+
     MyApplicationTheme {
         // A surface container using the 'background' color from the theme
         Surface(
